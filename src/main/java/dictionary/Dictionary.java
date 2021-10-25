12b8c44aee4ed35e6aaccc2689409216e8663a27
Cấn Quang Thịnh <91171206+C0Q0T@users.noreply.github.com>
2021-10-25 11:32:44 +0700

    Update Dictionary.java
    sua file dictionary

--- src/main/java/dictionary/Dictionary.java
+++ src/main/java/dictionary/Dictionary.java
@@ -8,64 +8,90 @@ public class Dictionary {
     private ArrayList<Word> words = new ArrayList<>();
 
     public void push(Word word) {
-        int length = words.size();
-        int index = searchIndexInsert(0, length - 1, word.getSpelling());
-        // if index = -1, word exist
-        if (index <= length && index >= 0) words.add(index, word);
-    }
-
-    private int searchIndexInsert(int start, int end, String spelling) {
-        if (end < start) return start;
-        int mid = start + (end - start) / 2;
-        if (mid == words.size()) return mid;
-        Word word = words.get(mid);
-        int compare = word.getSpelling().compareTo(spelling);
-        // word found
-        if (compare == 0) return -1;
-        if (compare > 0) return searchIndexInsert(start, mid - 1, spelling);
-        return searchIndexInsert(mid + 1, end, spelling);
-    }
-
-    // Find exactly word, return Word
-    private Word lookupWord(int start, int end, String spelling) {
-        if (end < start) return null;
-        int mid = start + (end - start) / 2;
-        Word word = words.get(mid);
-        String currentSpelling = word.getSpelling();
-        int compare = currentSpelling.compareTo(spelling);
-        if (compare == 0) return word;
-        if (compare > 0) return lookupWord(start, mid - 1, spelling);
-        return lookupWord(mid + 1, end, spelling);
-    }
-
-    // Find similar word, return int index of word in array.
-    private int searcherWord(int start, int end, String spelling) {
-        if (end < start) return -1;
-        int mid = start + (end - start) / 2;
-        Word word = words.get(mid);
-        String currentSpelling = word.getSpelling();
-        if (currentSpelling.startsWith(spelling)) {
-            return mid;
+        int len = words.size();
+        int vt = search_vt(0, len - 1, word.getSpelling());
+        if (vt <= len && vt >= 0) {
+            words.add(vt, word);
         }
-        int compare = currentSpelling.compareTo(spelling);
-        if (compare == 0) return mid;
-        if (compare > 0) return searcherWord(start, mid - 1, spelling);
-        return searcherWord(mid + 1, end, spelling);
+    }
+
+    private int search_vt(int start, int end, String spelling) {
+        if (end < start) {
+            return start;
+        }
+        while (start <= end) {
+            int mid = (start + end) / 2;
+            if (mid == words.size()) {
+                return mid;
+            }
+            Word word = words.get(mid);
+            int cmp = word.getSpelling().compareTo(spelling);
+            if (cmp == 0) {
+                return -1;
+            }
+            if (cmp > 0) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+        return start;
     }
 
     public Word lookup(String spelling) {
-        return lookupWord(0, words.size() - 1, spelling);
+
+        int len = words.size();
+        int start = 0;
+        int end = len - 1;
+        int mid;
+        while (start <= end) {
+            mid = (start + end) / 2;
+            Word word = words.get(mid);
+            String currentSpelling = word.getSpelling();
+            int cmp = currentSpelling.compareTo(spelling);
+            if (cmp == 0) {
+                return word;
+            } else if (cmp > 0) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+        return null;
+
     }
 
-    // Find an array of similar word.
     public ArrayList<Word> searcher(String spelling) {
         ArrayList<Word> result = new ArrayList<>();
-        int index =  searcherWord(0, words.size() - 1, spelling);
-
-        // if index = -1, not found word, return blank Array.
-        if (index >= 0) {
-            result.add(words.get(index));
-            int left = index - 1, right = index + 1;
+        int vt = -1;
+        int start = 0;
+        int end = words.size() - 1;
+        int mid;
+        if (start > end) {
+            vt = -1;
+        } else {
+            while (start <= end) {
+                mid = (start + end) / 2;
+                Word word = words.get(mid);
+                String currentSpelling = word.getSpelling();
+                if (currentSpelling.startsWith(spelling)) {
+                    vt = mid;
+                    break;
+                }
+                int cmp = currentSpelling.compareTo(spelling);
+                if (cmp == 0) {
+                    vt = mid;
+                    break;
+                } else if (cmp > 0) {
+                    end = mid - 1;
+                } else {
+                    start = mid + 1;
+                }
+            }
+        }
+        if (vt >= 0) {
+            result.add(words.get(vt));
+            int left = vt - 1, right = vt + 1;
 
             while (left >= 0) {
                 Word leftWord = words.get(left--);
@@ -74,8 +100,6 @@ public class Dictionary {
                 else
                     break;
             }
-
-//            TextField searchfield;
             int length = words.size();
             while (right < length) {
                 Word leftWord = words.get(right++);
@@ -88,11 +112,12 @@ public class Dictionary {
         return result;
     }
 
-    public ArrayList<Word> getWords() {
+    public ArrayList<Word> getWords () {
         return words;
     }
 
-    public void setWords(ArrayList<Word> words) {
+    public void setWords (ArrayList < Word > words) {
         this.words = words;
     }
+
 }
